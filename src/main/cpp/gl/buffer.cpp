f06e80ec3136065686620d7285250704dda6e6a7
Uniaball <2551733616@qq.com>
2025-09-06 21:11:33 +0800

    [Chore] (Debug): Debug

--- src/main/cpp/gl/buffer.cpp
+++ src/main/cpp/gl/buffer.cpp
@@ -11,7 +11,7 @@
 template <typename K, typename V>
 using unordered_map = ankerl::unordered_dense::map<K, V>;
 
-#define DEBUG 0
+#define DEBUG 1
 
 GLuint bound_array;
 static GLint maxBufferId = 0;
